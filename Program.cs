6247b52c0dd1be94ac435bef29f4624e0d99a28c
unknown <rfpimentel@outlook.com>
2020-11-08 23:13:21 -0300

    First commit!

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoMatematica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int quantidadeQuestoes;            
+            int respostasCorretas = 0;
+            
+            QuestoesMatematica questoes = new QuestoesMatematica();
+            
+            Console.WriteLine("JOGO MATEMÁTICA**************************\n");
+            Console.Write("Digite a quantidade de questões: ");
+            
+            Random random = new Random();
+            
+            quantidadeQuestoes = Int32.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < quantidadeQuestoes; i++)
+            {                
+                bool acertou;
+                
+                int sorteio = random.Next(4);                
+                
+                try
+                {                    
+                    switch (sorteio)
+                    {
+                        case 0:
+                            acertou = questoes.GerarQuestaoAdicao();
+                            if (acertou)
+                            {
+                                respostasCorretas++;
+                            }
+                            break;
+                        case 1:
+                            acertou = questoes.GerarQuestaoSubtracao();
+                            if (acertou)
+                            {
+                                respostasCorretas++;
+                            }
+                            break;
+                        case 2:
+                            acertou = questoes.GerarQuestaoMultiplicacao();
+                            if (acertou)
+                            {
+                                respostasCorretas++;
+                            }
+                            break;
+                        case 3:
+                            acertou = questoes.GerarQuestaoDivisao();
+                            if (acertou)
+                            {
+                                respostasCorretas++;
+                            }
+                            break;
+                    }
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Não é possível realizar divisão por 0.");
+                }               
+            }            
+            Console.WriteLine("\nVocê acertou a resposta em " + respostasCorretas + " de " + quantidadeQuestoes + " questões.");
+            Console.WriteLine("******************************************");            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QuestoesMatematica.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoMatematica
+{
+    public class QuestoesMatematica
+    {
+        private Random random = new Random();
+
+        public bool GerarQuestaoAdicao()
+        {            
+            bool acertou;                     
+            int numero1 = random.Next(51);            
+            int numero2 = random.Next(51);
+            
+            Console.Write(numero1 + " + " + numero2 + " = ");
+            
+            int resposta = Int32.Parse(Console.ReadLine());            
+            int resultado = numero1 + numero2;
+            
+            if (resultado == resposta)
+            {
+                acertou = true;
+            }
+            else
+            {
+                acertou = false;
+            }            
+            return acertou;
+        }
+        public bool GerarQuestaoSubtracao()
+        {
+            bool acertou;
+            int numero1 = random.Next(51);
+            int numero2 = random.Next(51);
+
+            Console.Write(numero1 + " - " + numero2 + " = ");
+
+            int resposta = Int32.Parse(Console.ReadLine());
+
+            int resultado = numero1 - numero2;
+
+            if (resultado == resposta)
+            {
+                acertou = true;
+            }
+            else
+            {
+                acertou = false;
+            }
+            return acertou;
+        }
+        public bool GerarQuestaoMultiplicacao()
+        {
+            bool acertou;
+            int numero1 = random.Next(11);
+            int numero2 = random.Next(11);
+
+            Console.Write(numero1 + " x " + numero2 + " = ");
+
+            int resposta = Int32.Parse(Console.ReadLine());
+
+            int resultado = numero1 * numero2;
+
+            if (resultado == resposta)
+            {
+                acertou = true;
+            }
+            else
+            {
+                acertou = false;
+            }
+            return acertou;
+        }
+        public bool GerarQuestaoDivisao()
+        {
+            bool acertou;
+            int numero1 = random.Next(11);
+            int numero2 = random.Next(11);
+            
+            Console.Write(numero1 + " / " + numero2 + " = ");
+
+            int resposta = Int32.Parse(Console.ReadLine());
+            
+            int resultado = numero1 / numero2;
+                                    
+            if (resultado == resposta)
+            {
+                acertou = true;
+            }
+            else
+            {
+                acertou = false;
+            }            
+            return acertou;            
+        }
+    }
+}
