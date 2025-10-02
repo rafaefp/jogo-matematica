08947238d8e2a1b5d03435f775c96ae658b815ba
Rafael F Pimentel <70771156+rafaefp@users.noreply.github.com>
2025-10-02 11:21:34 -0300

    Add AssemblyInfo.cs for project metadata

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JogoMatematica")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JogoMatematica")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Torna o assembly invisível para COM
+[assembly: ComVisible(false)]
+
+// GUID do typelib se exposto a COM (usar o mesmo do .csproj)
+[assembly: Guid("44D5DB69-1A5B-45B1-98EA-FC87DF35AD22")]
+
+// Versões (pode usar * para auto-incremento se quiser, ex: "1.0.*")
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
